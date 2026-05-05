a236d691870a8b98aa01329b6c728825486cc8c7
Chetan Anand <chetan108@outlook.com>
2026-05-05 19:26:56 +0530

    fix: db connection based on dev or prod.

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,6 +1,8 @@
 import { MongoClient } from "mongodb"
 
-const uri = process.env.MONGODB_URI
+const uri = process.env.NODE_ENV === "production" 
+  ? process.env.MONGODB_URI_PROD 
+  : process.env.MONGODB_URI_DEV
 
 if (!uri) throw new Error("MONGODB_URI is not set")
 
